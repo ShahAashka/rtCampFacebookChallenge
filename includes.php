22be8bad8d894e0b11214d5bcd5a87552c363450
ShahAashka <aashkashah71212@gmail.com>
2018-08-25 15:31:21 +0530

    Final Commit

--- includes.php
+++ includes.php
@@ -21,8 +21,8 @@
 	$fb_secret_id = 'b71488b55082d6fa027e82dd5df29a9a'; // Goto your Fb app->settings
 
 	//fb_login_url is same url which is added in facebook app->settings.
-	$fb_login_url = 'http://localhost/facebook1/'; 
-	$fb_logout_url = 'http://localhost/facebook1/logout.php';
+	$fb_login_url = 'https://rtcamp-facebook-challenge.herokuapp.com/'; 
+	$fb_logout_url = 'https://rtcamp-facebook-challenge.herokuapp.com/logout.php';
 
 	session_start();
 
